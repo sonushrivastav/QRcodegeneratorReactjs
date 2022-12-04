c154a6c3b5ce986b8622c8a4c1a4e45e4a8fb37f
shrivastav1811 <sonushrivastav1811@gmail.com>
2022-12-04 18:58:10 +0530

    qrcode genrator commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { React, useEffect, useState } from "react";
 
 function App() {
+  const [click, setClick] = useState(false);
+  const [inputChange, setInputchange] = useState("");
+  const [imgsrc, setImgsrc] = useState();
+  const [loading, setLoading] = useState();
+  const handleclick = (e) => {
+    
+      
+    
+      console.log("key press");
+      if (!inputChange) return;
+      setClick(true);
+      // setImgsrc(
+      //   `https://api.qrserver.com/v1/create-qr-code/?size=170x170&data=${inputChange}`
+      // )
+      setLoading("Generating QR Code...");
+      fetch(
+        `https://api.qrserver.com/v1/create-qr-code/?size=170x170&data=${inputChange}`
+      ).then((data) => {
+        setLoading("Generate QR Code");
+        setImgsrc(data.url);
+      });
+    
+  };
+
+  const removeQrContainer = () => {
+    if (!inputChange && click) {
+      setClick(false);
+    }
+  };
+
+  useEffect(() => {
+    removeQrContainer();
+  }, [click, inputChange]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className={click ? "Wrapper active" : "Wrapper"}>
+      <header>
+        <h1>QR Code Generator</h1>
+        <p>Paste a url or enter text to create QR code</p>
       </header>
+      <div className="form">
+        <input
+          type="text"
+          onChange={(e) => setInputchange(e.target.value)}
+          placeholder="Enter text or url"
+        />
+        <button onClick={(e)=>handleclick(e)}>
+          {loading ? loading : "Generate QR Code"}
+        </button>
+      </div>
+      <div className="qr_code">
+        <img src={imgsrc} alt="" />
+      </div>
     </div>
   );
 }
